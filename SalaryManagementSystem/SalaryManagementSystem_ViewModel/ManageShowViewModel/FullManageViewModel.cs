15b1797cd5821a42e8bff8632e5ddae7672f1b08
AmirDeviloper <amirdevil.main@gmail.com>
2025-10-22 13:36:28 +0330

    Add files via upload

---
+++ SalaryManagementSystem/SalaryManagementSystem_ViewModel/ManageShowViewModel/FullManageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryManagementSystem_ViewModel.ManageShowViewModel
+{
+    public class FullManageViewModel
+    {
+        public int ID { get; set; }
+        public int DID { get; set; }
+        public decimal Salary { get; set; }
+        public string EID { get; set; }
+        public string Rank { get; set; }
+
+        public string EName { get; set; }
+        public string DName { get; set; }
+    }
+}
